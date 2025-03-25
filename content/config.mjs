0e2ab6abe616b229b58924ab97e14dbb8f615e76
lulll123 <luzhixiong9975@outlook.com>
2025-03-25 09:55:55 +0800

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -2,7 +2,8 @@ const config = {
   title: "My Awesome Blog",
   description: "This is my awesome blog built with Flowershow",
   author: "John",
-  domain: "https://fs.mynoteshub.xyz"
+  domain: "https://fs.mynoteshub.xyz",
+  showsidebar: true
 };
 
 export default config;
