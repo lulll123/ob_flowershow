e0f1d0b72989e66a3cda520451af9a9746a69f27
lulll123 <luzhixiong9975@outlook.com>
2025-03-25 09:10:46 +0800

    Add files via upload

---
+++ content/config.mjs
@@ -0,0 +1,8 @@
+const config = {
+  title: "My Awesome Blog",
+  description: "This is my awesome blog built with Flowershow",
+  author: "John",
+  domain: "https://fs.mynoteshub.xyz"
+};
+
+export default config;
