c95fac8035b60602c3a3098d6ad22022ee0f5e3e
lulll123 <luzhixiong9975@outlook.com>
2025-03-25 09:08:26 +0800

    Delete content directory

--- content/config.mjs
+++
@@ -1,8 +0,0 @@
-const config = {
-  title: "My Awesome Blog",
-  description: "This is my awesome blog built with Flowershow",
-  author: "John",
-  domain: "https://fs.mynoteshub.xyz"
-};
-
-export default config;
